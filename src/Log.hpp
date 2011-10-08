6994cbcd55f78e160876c79b216b06e1538ff01e
Long Sean <longxjgm@gmail.com>
2011-10-08 21:50:28 +0800

    add code

--- src/Log.hpp
+++ src/Log.hpp
@@ -21,6 +21,11 @@ private:
 	FILE* m_pstFile;
 };
 
-#define LOG(iLogLevel, szLogStr, ...) {CSingleton<CLog>::Instance()->Dolog(iLogLevel, __FILE__, __LINE__, __FUNCTION__, szLogStr, __VA_ARGS__);}
+
+#define LOG_DEBUG(szLogStr, ...) {CSingleton<CLog>::Instance()->Dolog(CLog::LEVEL_DEBUG, __FILE__, __LINE__, __FUNCTION__, szLogStr, __VA_ARGS__);}
+
+#define LOG_INFO(szLogStr, ...) {CSingleton<CLog>::Instance()->Dolog(CLog::LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, szLogStr, __VA_ARGS__);}
+
+#define LOG_ERROR(szLogStr, ...) {CSingleton<CLog>::Instance()->Dolog(CLog::LEVEL_ERROR, __FILE__, __LINE__, __FUNCTION__, szLogStr, __VA_ARGS__);}
 
 #endif
