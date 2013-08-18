be9520989f4569267043efdf822959746194e405
sean long <longxjgm@gmail.com>
2013-08-17 21:45:55 -0700

    finish spscqueue

--- src/lib/MyQueue.hpp
+++ src/lib/MyQueue.hpp
@@ -37,18 +37,25 @@ public:
 			return true;
 		}
 
-	void GetData(uint64_t index, DataType& data)
+	// 线程安全
+public:
+	inline void GetData(uint64_t index, DataType& data)
 		{
 			data = buffer_[index & mask_];
 		}
 
 	
-	void SetData(uint64_t index, const DataType& data)
+	inline void SetData(uint64_t index, const DataType& data)
 		{
 			buffer_[index & mask_] = data;
 		}
+
+	inline DataType* GetSlot(uint64_t index)
+		{
+			return &buffer_[index & mask_];
+		}
 	
-	uint64_t Capacity()
+	inline uint64_t Capacity()
 		{
 			return mask_ + 1;
 		}
@@ -61,23 +68,31 @@ private:
 template<class DataType>
 class IConsumer
 {
+	// 阻塞接口
 public:
-	void Consume(DataType& data) = 0;
+	virtual void Consume(DataType& data) = 0;
+
+	virtual DataType* GetSlot() = 0;
+	virtual void Commit() = 0;	
 };
 
 template<class DataType>
 class IProducer
 {
+	// 阻塞接口
 public:
-	void Produce(const DataType& data) = 0;
+	virtual void Produce(const DataType& data) = 0;
+
+	virtual DataType* GetSlot() = 0;
+	virtual void Commit() = 0;
 };
 
 template<class DataType>
 class IQueue
 {
 public:
-	IConsumer* GetConsumer() = 0;
-	IProducer* GetProducer() = 0;
+	virtual IConsumer* GetConsumer() = 0;
+	virtual IProducer* GetProducer() = 0;
 };
 
 template<class DataType>
@@ -96,18 +111,50 @@ public:
 		}
 	
 public:
-	void Consume(DataType& data)
+	virtual void Consume(DataType& data)
 		{
+			WaitForReady();
+			
+			queue_->GetData(pos_, data);
+			pos_++;
 		}
 
-	uint64_t pos()
+	virtual DataType* GetSlot()
+		{
+			WaitForReady();
+
+			return queue_->GetSlot(pos_);
+		}
+	
+	virtual void Commit()
+		{
+			pos_++;
+		}
+
+	inline uint64_t pos()
 		{
 			return pos_;
 		}
 
 private:
+	inline void WaitForReady()
+		{
+			while (pos_ >= end_cache_)
+			{
+				UpdateEndCache();
+			}
+		}
+	
+	inline void UpdateEndCache()
+		{
+			end_cache_ = queue->GetProducerPos();
+		}
+
+private:
 	volatile uint64_t pos_;
 	char padding[CACHE_LEN - sizeof(pos_)];
+	uint64_t end_cache_;
+	char padding1[CACHE_LEN - sizeof(end_cache_)];
 	SPSCQueue<DataType>* queue_;
 };
 
@@ -116,11 +163,29 @@ class SPSCProducer : public IProducer
 {
 	// block: 需要保证线程安全
 public:
-	void Produce(const DataType& data)
+	virtual void Produce(const DataType& data)
 		{
+			WaitForReady();
+			
+			queue_->SetData(pos_, data);
+			asm volatile("sfence":::"memory");
+			pos_++;
 		}
 
-	uint64_t pos()
+	virtual DataType* GetSlot()
+		{
+			WaitForReady();
+			
+			return queue->GetSlot(pos_);
+		}
+
+	virtual void Commit()
+		{
+			asm volatile("sfence":::"memory");
+			pos_++;
+		}
+
+	inline uint64_t pos()
 		{
 			return pos_;
 		}
@@ -140,7 +205,15 @@ public:
 		}
 
 private:
-	void UpdateEndCache()
+	inline void WaitForReady()
+		{
+			while (pos_ >= end_cache_)
+			{
+				UpdateEndCache();
+			}
+		}
+	
+	inline void UpdateEndCache()
 		{
 			end_cache_ = queue->GetConsumerPos() + queue->GetCapacity();
 		}
@@ -202,27 +275,42 @@ public:
 
 	// 线程安全
 public:
-	uint64_t Capacity()
+	inline uint64_t Capacity()
 		{
 			return buffer_.Capacity();
 		}
 
 	// 非实时数据
-	uint64_t Size()
+	inline uint64_t Size()
 		{
 			return producer_.pos() - consumer_.pos();
 		}
 
 	// 非实时
-	uint64_t GetConsumerPos()
+	inline uint64_t GetConsumerPos()
 		{
 			return consumer_.pos();
 		}
 
 	// 非实时
-	uint64_t GetProducerPos()
+	inline uint64_t GetProducerPos()
 		{
 			return producer_.pos();
+		}
+
+	inline void GetData(uint64_t index, DataType& data)
+		{
+			buffer_.GetData(index, data);
+		}
+
+	inline void SetData(uint64_t index, const DataType& data)
+		{
+			buffer_.SetData(index, data);
+		}
+
+	inline DataType* GetSlot(uint64_t index)
+		{
+			return buffer_.GetSlot(index);
 		}
 
 private:
